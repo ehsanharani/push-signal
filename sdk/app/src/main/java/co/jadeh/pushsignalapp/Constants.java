b3a3c1edd67deec0d4e1994fd9f522837afeebfa
ehsan.harani <ehsan.harani@gmail.com>
2019-11-27 12:46:45 +0330

    fix bugs

--- sdk/app/src/main/java/co/jadeh/pushsignalapp/Constants.java
+++ sdk/app/src/main/java/co/jadeh/pushsignalapp/Constants.java
@@ -3,11 +3,11 @@ package co.jadeh.pushsignalapp;
 public class Constants {
 
     //also set this app_id in app/build.gradle into onesignal_app_id
-    public final static String APP_ID = "8617a4a8-dbf2-430a-b124-b6c34f2f01bb";
+    public final static String APP_ID = "app-id";
 
     //jadeh api-key
-    public final static String API_KEY = "$2b$15$EXIWtQZHO1RESEInrMCq1uvyUg6bHxMdaABu7UR7qkuHkLpPsuC66";
+    public final static String API_KEY = "jadeh-api-key";
 
     //you can set this google_number in app/build.gradle into onesignal_google_project_number
-    public final static String GOOGLE_NUMBER = "52335722234";
+    public final static String GOOGLE_NUMBER = "1234567";
 }
